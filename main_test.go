d84317301bff373d1535bbce525ac9449af19f1c
Adam Pickering <adam.pickering@suse.com>
2025-03-04 15:30:57 -0700

    Implement getRegsyncEntries unit test as Diogo suggested

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/rancher/image-mirror/pkg/config"
@@ -10,44 +9,64 @@ import (
 )
 
 func TestGetRegsyncEntries(t *testing.T) {
-	for _, targetImageName := range []string{"", "other-org-test-image"} {
-		t.Run(fmt.Sprintf("should return correct regsync entries with TargetImageName set to %q", targetImageName), func(t *testing.T) {
-			sourceOrg := "test-org"
-			sourceImageName := "test-image"
-			expectedTargetImageName := targetImageName
-			if targetImageName == "" {
-				expectedTargetImageName = "mirrored-" + sourceOrg + "-" + sourceImageName
-			}
-			image := config.Image{
-				SourceImage:     sourceOrg + "/" + sourceImageName,
-				TargetImageName: targetImageName,
+	type TestCase struct {
+		Name            string
+		TargetImageName string
+		ExpectedEntries []regsync.ConfigSync
+	}
+	for _, testCase := range []TestCase{
+		{
+			Name:            "should use default image name when TargetImageName is not set",
+			TargetImageName: "",
+			ExpectedEntries: []regsync.ConfigSync{
+				{
+					Source: "test-org/test-image:v1.2.3",
+					Target: "docker.io/test1/mirrored-test-org-test-image:v1.2.3",
+					Type:   "image",
+				},
+				{
+					Source: "test-org/test-image:v2.3.4",
+					Target: "docker.io/test1/mirrored-test-org-test-image:v2.3.4",
+					Type:   "image",
+				},
+			},
+		},
+		{
+			Name:            "should use TargetImageName when it is set",
+			TargetImageName: "other-org-test-image",
+			ExpectedEntries: []regsync.ConfigSync{
+				{
+					Source: "test-org/test-image:v1.2.3",
+					Target: "docker.io/test1/other-org-test-image:v1.2.3",
+					Type:   "image",
+				},
+				{
+					Source: "test-org/test-image:v2.3.4",
+					Target: "docker.io/test1/other-org-test-image:v2.3.4",
+					Type:   "image",
+				},
+			},
+		},
+	} {
+		t.Run(testCase.Name, func(t *testing.T) {
+			inputImage := config.Image{
+				SourceImage:     "test-org/test-image",
+				TargetImageName: testCase.TargetImageName,
 				Tags: []string{
 					"v1.2.3",
 					"v2.3.4",
 				},
 			}
-			repo := config.Repository{
+			inputRepository := config.Repository{
 				EnvVarPrefix: "TEST1",
 				BaseUrl:      "docker.io/test1",
 			}
-			regsyncEntries, err := getRegsyncEntries(repo, image)
+			regsyncEntries, err := getRegsyncEntries(inputRepository, inputImage)
 			if err != nil {
 				t.Fatalf("unexpected error: %s", err)
 			}
-			assert.Len(t, regsyncEntries, 2)
-			expectedEntries := []regsync.ConfigSync{
-				{
-					Source: "test-org/test-image:v1.2.3",
-					Target: "docker.io/test1/" + expectedTargetImageName + ":v1.2.3",
-					Type:   "image",
-				},
-				{
-					Source: "test-org/test-image:v2.3.4",
-					Target: "docker.io/test1/" + expectedTargetImageName + ":v2.3.4",
-					Type:   "image",
-				},
-			}
-			for _, expectedEntry := range expectedEntries {
+			assert.Len(t, regsyncEntries, len(testCase.ExpectedEntries))
+			for _, expectedEntry := range testCase.ExpectedEntries {
 				assert.Contains(t, regsyncEntries, expectedEntry)
 			}
 		})
