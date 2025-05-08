72e7c7ee08eee59d018dbde2658bc3d3fe4eec3d
Adam Pickering <adam.pickering@suse.com>
2025-05-08 14:49:34 -0600

    Add autoupdate.yaml to subjects of format subcommand

--- tools/internal/autoupdate/config.go
+++ tools/internal/autoupdate/config.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 
 	"github.com/rancher/image-mirror/internal/config"
 
@@ -27,6 +29,23 @@ func Parse(filePath string) ([]ConfigEntry, error) {
 	}
 
 	return config, nil
+}
+
+func Write(filePath string, config []ConfigEntry) error {
+	slices.SortStableFunc(config, func(a, b ConfigEntry) int {
+		return strings.Compare(a.Name, b.Name)
+	})
+
+	contents, err := yaml.Marshal(config)
+	if err != nil {
+		return fmt.Errorf("failed to marshal: %w", err)
+	}
+
+	if err := os.WriteFile(filePath, contents, 0o644); err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+
+	return nil
 }
 
 func (entry ConfigEntry) GetLatestImages() ([]*config.Image, error) {
