2239159e5f1a0c5206f64924d8afb0a73d46283c
Adam Pickering <adam.pickering@suse.com>
2025-05-20 16:57:17 -0600

    Move tracking of file paths into its own package

---
+++ tools/internal/paths/paths.go
@@ -0,0 +1,7 @@
+package paths
+
+var AutoUpdateYaml = "autoupdate.yaml"
+var ConfigJson = "retrieve-image-tags/config.json"
+var ConfigYaml = "config.yaml"
+var ImagesList = "images-list"
+var RegsyncYaml = "regsync.yaml"
