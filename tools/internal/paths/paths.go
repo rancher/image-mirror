e1923fae2722db49741b1de900dae7f7d5925d39
Adam Pickering <adam.pickering@suse.com>
2025-05-30 17:16:38 -0600

    Make AutoUpdateYaml, ConfigJson and RegsyncYaml const

--- tools/internal/paths/paths.go
+++ tools/internal/paths/paths.go
@@ -1,7 +1,8 @@
 package paths
 
-var AutoUpdateYaml = "autoupdate.yaml"
-var ConfigJson = "retrieve-image-tags/config.json"
+const AutoUpdateYaml = "autoupdate.yaml"
+const ConfigJson = "retrieve-image-tags/config.json"
+const RegsyncYaml = "regsync.yaml"
+
 var ConfigYaml = "config.yaml"
 var ImagesList = "images-list"
-var RegsyncYaml = "regsync.yaml"
