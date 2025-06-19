677dec5dc6b70f09bdea20ea00d29f2f70989693
Adam Pickering <adam.pickering@suse.com>
2025-06-19 15:07:18 -0600

    Add HelmLatest type

--- tools/go.mod
+++ tools/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/google/go-github/v71 v71.0.0
 	github.com/stretchr/testify v1.10.0
 	github.com/urfave/cli/v3 v3.3.3
+	gopkg.in/yaml.v3 v3.0.1
 	sigs.k8s.io/yaml v1.4.0
 )
 
@@ -16,5 +17,4 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ tools/internal/autoupdate/helmlatest.go
@@ -0,0 +1,165 @@
+package autoupdate
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os/exec"
+	"slices"
+	"strings"
+
+	"github.com/rancher/image-mirror/internal/config"
+	"gopkg.in/yaml.v3"
+)
+
+const helmRepoName = "image-mirror-tools-temp"
+
+// An Environment is a set of configuration we would like to apply to a
+// chart when templating it out and searching the result for images.
+type Environment []string
+
+// ToHelmTemplateArgs converts the environment to helm template --set arguments
+func (e Environment) ToHelmTemplateArgs() []string {
+	args := make([]string, 0, len(e)*2)
+	for _, value := range e {
+		args = append(args, "--set", value)
+	}
+	return args
+}
+
+// HelmLatest retrieves image references by templating the latest version of the
+// configured helm chart, and recursively finding all fields with an "image" key.
+// This is not a perfectly reliable way of finding images that a chart uses, and
+// does not attempt to be. However, it is probably good enough for simple charts.
+type HelmLatest struct {
+	// HelmRepo is the URL of the Helm chart repository.
+	HelmRepo string
+	// Charts is a map of chart names to the environments under which we want to
+	// search them for images.
+	Charts map[string]map[string]Environment
+	// ImageDenylist is a list of images to exclude from the result.
+	ImageDenylist []string `json:",omitempty"`
+}
+
+// GetUpdateImages templates the helm chart and extracts all image references
+func (hl *HelmLatest) GetUpdateImages() ([]*config.Image, error) {
+	if _, err := exec.LookPath("helm"); err != nil {
+		return nil, fmt.Errorf("helm command not found: %w", err)
+	}
+
+	imageMap := make(map[string][]string)
+
+	addRepoCmd := exec.Command("helm", "repo", "add", helmRepoName, hl.HelmRepo)
+	if err := addRepoCmd.Run(); err != nil {
+		return nil, fmt.Errorf("failed to add helm repository: %w", err)
+	}
+	defer exec.Command("helm", "repo", "remove", helmRepoName).Run()
+
+	updateCmd := exec.Command("helm", "repo", "update")
+	if err := updateCmd.Run(); err != nil {
+		return nil, fmt.Errorf("failed to update helm repositories: %w", err)
+	}
+
+	for chartName, environmentMap := range hl.Charts {
+		for environmentName, environment := range environmentMap {
+			args := []string{
+				"template", chartName + "-" + environmentName,
+				helmRepoName + "/" + chartName,
+			}
+			args = append(args, environment.ToHelmTemplateArgs()...)
+
+			templateCmd := exec.Command("helm", args...)
+			templateOutput := &bytes.Buffer{}
+			templateCmd.Stdout = templateOutput
+			if err := templateCmd.Run(); err != nil {
+				return nil, fmt.Errorf("failed to template chart %s for env %s: %w", chartName, environmentName, err)
+			}
+
+			decoder := yaml.NewDecoder(templateOutput)
+			for {
+				var templateData any
+				if err := decoder.Decode(&templateData); errors.Is(err, io.EOF) {
+					break
+				} else if err != nil {
+					return nil, fmt.Errorf("failed to parse template command output as yaml: %w", err)
+				}
+				if err := hl.extractImagesFromYaml(templateData, imageMap); err != nil {
+					return nil, fmt.Errorf("failed to extract images from chart %s env %s: %w", chartName, environmentName, err)
+				}
+			}
+		}
+	}
+
+	// Convert the map to a slice of config.Image objects
+	images := make([]*config.Image, 0, len(imageMap))
+	for sourceImage, tags := range imageMap {
+		image, err := config.NewImage(sourceImage, tags)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create image: %w", err)
+		}
+		images = append(images, image)
+	}
+
+	// Filter out denied images
+	filteredImages := make([]*config.Image, 0, len(images))
+	for _, image := range images {
+		if !slices.Contains(hl.ImageDenylist, image.SourceImage) {
+			filteredImages = append(filteredImages, image)
+		}
+	}
+
+	return filteredImages, nil
+}
+
+func (hl *HelmLatest) extractImagesFromYaml(data any, imageMap map[string][]string) error {
+	switch v := data.(type) {
+	case map[string]any:
+		for key, value := range v {
+			if key == "image" {
+				stringValue, ok := value.(string)
+				if ok {
+					repository, tag, err := hl.parseImageRef(stringValue)
+					if err != nil {
+						return fmt.Errorf("failed to parse %q as image ref: %w", stringValue, err)
+					}
+					existingTags, present := imageMap[repository]
+					if !present {
+						imageMap[repository] = []string{tag}
+					} else if !slices.Contains(existingTags, tag) {
+						imageMap[repository] = append(imageMap[repository], tag)
+					}
+				}
+			} else {
+				if err := hl.extractImagesFromYaml(value, imageMap); err != nil {
+					return err
+				}
+			}
+		}
+	case []any:
+		for _, item := range v {
+			if err := hl.extractImagesFromYaml(item, imageMap); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (hl *HelmLatest) parseImageRef(rawString string) (string, string, error) {
+	parts := strings.Split(rawString, "@")
+	if len(parts) > 1 {
+		return "", "", fmt.Errorf("%q has digest, which is not supported", rawString)
+	}
+	withoutDigest := parts[0]
+
+	parts = strings.Split(withoutDigest, ":")
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf(`failed to split %q into two parts on ":"`, withoutDigest)
+	}
+	repository := parts[0]
+	tag := parts[1]
+	repositoryWithoutDocker := strings.TrimPrefix(repository, "docker.io/")
+
+	return repositoryWithoutDocker, tag, nil
+}
