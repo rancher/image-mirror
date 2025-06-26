6d753c6db6eee05667ab2b911e7353d28e1b4b1f
Diogo Souza <diogo.souza@suse.com>
2025-06-26 19:35:03 -0300

    adding githubRelease strategy

--- tools/internal/autoupdate/githublatestrelease.go
+++ tools/internal/autoupdate/githublatestrelease.go
@@ -5,31 +5,90 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/rancher/image-mirror/internal/config"
-
+	"github.com/Masterminds/semver/v3"
 	"github.com/google/go-github/v71/github"
+	"github.com/rancher/image-mirror/internal/config"
 )
 
-// GithubLatestRelease retrieves the tag of the latest release
-// of the configured github repository, and returns the configured
-// Images with this tag. It assumes that the images have the same
-// tags as the github releases.
-type GithubLatestRelease struct {
-	Owner      string
-	Repository string
-	Images     []AutoupdateImageRef
+// GithubRelease retrieves the tags of all github releases that match the VersionConstraint if
+// LatestOnly is false, or the tag of the latest release if LatestOnly is true.
+// It returns the configured Images with this tag.
+// It assumes that the images have the same tags as the github releases.
+type GithubRelease struct {
+	Owner             string
+	Repository        string
+	Images            []AutoupdateImageRef
+	LatestOnly        bool
+	VersionConstraint string
 }
 
-func (glr *GithubLatestRelease) GetUpdateImages() ([]*config.Image, error) {
+func (gr *GithubRelease) GetUpdateImages() ([]*config.Image, error) {
 	client := github.NewClient(nil)
-	release, _, err := client.Repositories.GetLatestRelease(context.Background(), glr.Owner, glr.Repository)
+	if gr.LatestOnly {
+		return gr.getImagesFromLatestRelease(client)
+	} else {
+		return gr.getImagesFromAllReleases(client)
+	}
+}
+
+func (gr *GithubRelease) getImagesFromAllReleases(client *github.Client) ([]*config.Image, error) {
+	opt := &github.ListOptions{}
+
+	images := make([]*config.Image, 0, len(gr.Images))
+	for {
+		releases, resp, err := client.Repositories.ListReleases(context.Background(), gr.Owner, gr.Repository, opt)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get releases: %w", err)
+		}
+
+		for _, release := range releases {
+			if release.GetPrerelease() || release.GetDraft() {
+				continue
+			}
+			if gr.VersionConstraint != "" {
+				version, err := semver.NewVersion(release.GetTagName())
+				if err != nil {
+					return nil, fmt.Errorf("error parsing release version: %w", err)
+				}
+
+				constraint, err := semver.NewConstraint(gr.VersionConstraint)
+				if err != nil {
+					return nil, fmt.Errorf("error parsing version constraint: %w", err)
+				}
+
+				//check if tag matches the constraint
+				if !constraint.Check(version) {
+					continue
+				}
+			}
+			for _, sourceImage := range gr.Images {
+				image, err := config.NewImage(sourceImage.SourceImage, []string{release.GetTagName()})
+				if err != nil {
+					return nil, fmt.Errorf("failed to construct image from source image %q and tag %q: %w", sourceImage, release.GetTagName(), err)
+				}
+				image.SetTargetImageName(sourceImage.TargetImageName)
+				images = append(images, image)
+			}
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+
+	return images, nil
+}
+
+func (gr *GithubRelease) getImagesFromLatestRelease(client *github.Client) ([]*config.Image, error) {
+	release, _, err := client.Repositories.GetLatestRelease(context.Background(), gr.Owner, gr.Repository)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get latest release: %w", err)
 	}
 	latestTag := *release.TagName
 
-	images := make([]*config.Image, 0, len(glr.Images))
-	for _, sourceImage := range glr.Images {
+	images := make([]*config.Image, 0, len(gr.Images))
+	for _, sourceImage := range gr.Images {
 		image, err := config.NewImage(sourceImage.SourceImage, []string{latestTag})
 		if err != nil {
 			return nil, fmt.Errorf("failed to construct image from source image %q and tag %q: %w", sourceImage, latestTag, err)
@@ -41,17 +100,26 @@ func (glr *GithubLatestRelease) GetUpdateImages() ([]*config.Image, error) {
 	return images, nil
 }
 
-func (glr *GithubLatestRelease) Validate() error {
-	if glr.Owner == "" {
+func (gr *GithubRelease) Validate() error {
+	if gr.Owner == "" {
 		return errors.New("must specify Owner")
 	}
-	if glr.Repository == "" {
+	if gr.Repository == "" {
 		return errors.New("must specify Repository")
 	}
-	if glr.Images == nil {
+	if gr.Images == nil {
 		return errors.New("must specify Images")
-	} else if len(glr.Images) == 0 {
+	} else if len(gr.Images) == 0 {
 		return errors.New("must specify at least one element for Images")
+	}
+	if gr.LatestOnly && gr.VersionConstraint != "" {
+		return errors.New("must not specify VersionConstraint when LatestOnly=true")
+	}
+	if gr.VersionConstraint != "" {
+		_, err := semver.NewConstraint(gr.VersionConstraint)
+		if err != nil {
+			return fmt.Errorf("invalid VersionConstraint: %w", err)
+		}
 	}
 	return nil
 }
